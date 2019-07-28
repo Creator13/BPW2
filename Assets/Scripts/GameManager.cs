a24320bac7bba6f90d5acb7ad1a4e0be2401a696
Casper van Battum <cvanbattum@gmail.com>
2019-07-29 00:14:37 +0200

    Added chance for seed yield

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,7 +18,11 @@ public class GameManager : MonoBehaviour {
 	public string MainMenuScene => mainMenuScene;
 
 	[SerializeField] private bool isLastLevel;
-	
+
+	[Space(10)]
+	[Range(0, 1), SerializeField]
+	private float seedYieldChance = .2f;
+
 	[Space(10)] [SerializeField] private List<ExportGoal> goals;
 	public List<ExportGoal> Goals => goals;
 	public Species[] Species {
@@ -30,7 +34,7 @@ public class GameManager : MonoBehaviour {
 			return s.ToArray();
 		}
 	}
-	
+
 	private bool paused;
 	public bool Paused {
 		get => paused;
@@ -137,6 +141,16 @@ public class GameManager : MonoBehaviour {
 	public void RegisterHarvest(Species s, int amt) {
 		// Count the harvested species towards the goal in the list of goals.
 		Goals.Find(goal => goal.Species == s).Current += amt;
+
+		YieldSeeds(s);
+	}
+
+	public void YieldSeeds(Species s) {
+		// TODO Make this species dependent
+		// Give the player seeds at random, based on a chance setting
+		if (Random.value <= seedYieldChance) {
+			Goals.Find(goal => goal.Species == s).Seeds += 1;
+		}
 	}
 
 	public bool HasSeed(Species s) {
